bae6938e82d82a860399bc307c4f4feeb392e08a
Elizabeth Absecon <Ficapy@Ficapy.local>
2015-07-16 16:45:05 +0800

    日了狗了🐶  ,'高贵'的osx自带iconv不支持-o输出参数

--- convert_file_to_utf-8.py
+++ convert_file_to_utf-8.py
@@ -5,10 +5,10 @@
 
 # FUCK YOU GB2312
 
+import codecs
 import os
 import shutil
 from chardet.universaldetector import UniversalDetector
-from subprocess import Popen
 from os import path
 
 dir = path.dirname(path.abspath(__file__))
@@ -30,9 +30,17 @@ for root, dirnames, files in os.walk(dir):
         os.makedirs(utf8path, exist_ok=True)
         dst = path.join(utf8path, file)
 
-        shutil.copyfile(filepath, dst)
+        # http://stackoverflow.com/questions/191359/how-to-convert-a-file-to-utf-8-in-python
+        BLOCKSIZE = 1048576  # or some other, desired size in bytes
         if encode:
-            Popen(['iconv', '-f', encode, '-t', 'UTF-8', filepath, '-o', dst], shell=True)
-
-
-
+            with codecs.open(filepath, "r", encode) as sourceFile:
+                with codecs.open(dst, "w", "utf-8") as targetFile:
+                    while True:
+                        contents = sourceFile.read(BLOCKSIZE)
+                        if not contents:
+                            break
+                        targetFile.write(contents)
+                        # 日了狗了🐶，osx上自带iconv不支持-o参数
+                        # subprocess.call(['iconv', '-f', encode, '-t', 'UTF-8', filepath, '-o', dst],stdout=subprocess.DEVNULL)
+        else:
+            shutil.copyfile(filepath, dst)
